cf9e11fe0bb2b1b8e212bd32d45a2036696343a5
Guilherme Fagundes <guilhermelemosfurg@gmail.com>
2025-03-21 16:22:41 -0300

    feat: add validation to password recover

--- src/Pages/PasswordReset.tsx
+++ src/Pages/PasswordReset.tsx
@@ -10,15 +10,60 @@ const PasswordReset = () => {
   const [code, setCode] = useState("");
   const [verifying, setVerifying] = useState(false);
   const [error, setError] = useState("");
-  const { isLoaded, signIn, setActive } = useSignIn();
-  const { isSignedIn } = useUser();
+  const { isLoaded, signIn } = useSignIn();
   const navigate = useNavigate();
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [emailError, setEmailError] = useState('')
+
+  const validateEmail = (email: string) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const validatePassword = (password: string) => {
+    if (!password) {
+      setPasswordError("Password is required.");
+      return false;
+    } else if (
+      !/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+        password
+      )
+    ) {
+      setPasswordError(
+        "Password must contain at least 8 characters, 1 uppercase letter, 1 number and 1 special character."
+      );
+      return false;
+    } else {
+      setPasswordError("");
+      return true;
+    }
+  };
+
+  const validateConfirmPassword = (
+    password: string,
+    repeatPassword: string
+  ) => {
+    if (password !== repeatPassword) {
+      setConfirmPasswordError("Passwords do not match.");
+      return false;
+    } else {
+      setConfirmPasswordError("");
+      return true;
+    }
+  };
 
   async function create(e: FormEvent) {
     e.preventDefault();
     if (!isLoaded) {
       return;
     }
+    if (!validateEmail(email)) {
+      e.preventDefault();
+      setEmailError("Please Provide a valid email")
+      return;
+    }
+    setEmailError("")
 
     await signIn
       ?.create({
@@ -69,49 +114,77 @@ const PasswordReset = () => {
         >
           {verifying && (
             <>
-              <label
-                htmlFor="password"
-                className="font-inter text-bl-600 text-p1 font-medium"
-              >
-                New Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                placeholder="Your New Password"
-                className=" border-1 border-bl-100 rounded-md px-2 h-11 outline-none"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <label
-                htmlFor="passwordRepeat"
-                className="font-inter text-bl-600 text-p1 font-medium"
-              >
-                Repeat Password
-              </label>
-              <input
-                type="password"
-                id="passwordRepeat"
-                placeholder="Repeat Your Password"
-                className="border-1 border-bl-100 rounded-md px-2 h-11 outline-none"
-                onChange={(e) => setRepeatPassword(e.target.value)}
-              />
-              <label
-                htmlFor="code"
-                className="font-inter text-bl-600 text-p1 font-medium"
-              >
-                Code
-              </label>
-              <input
-                type="text"
-                id="code"
-                placeholder="Code"
-                className="border-1 border-bl-100 rounded-md px-2 h-11 outline-none"
-                onChange={(e) => setCode(e.target.value)}
-              />
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor="password"
+                  className="font-inter text-bl-600 text-p1 font-medium"
+                >
+                  New Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Your New Password"
+                  className=" border-1 border-bl-100 rounded-md px-2 h-11 outline-none"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    validatePassword(e.target.value);
+                  }}
+                />
+                {passwordError && (
+                  <p className="font-inter text-l1 text-r-900 text-center">
+                    {passwordError}
+                  </p>
+                )}
+              </div>
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor="passwordRepeat"
+                  className="font-inter text-bl-600 text-p1 font-medium"
+                >
+                  Repeat Password
+                </label>
+                <input
+                  type="password"
+                  id="passwordRepeat"
+                  placeholder="Repeat Your Password"
+                  className="border-1 border-bl-100 rounded-md px-2 h-11 outline-none"
+                  onChange={(e) => {
+                    setRepeatPassword(e.target.value);
+                    validateConfirmPassword(password, e.target.value);
+                  }}
+                />
+                {confirmPasswordError && (
+                  <p className="font-inter text-l1 text-r-900 text-center">
+                    {confirmPasswordError}
+                  </p>
+                )}
+              </div>
+              <div className="flex flex-col gap-1">
+                <label
+                  htmlFor="code"
+                  className="font-inter text-bl-600 text-p1 font-medium"
+                >
+                  Code
+                </label>
+                <input
+                  type="text"
+                  id="code"
+                  placeholder="Code"
+                  className="border-1 border-bl-100 rounded-md px-2 h-11 outline-none"
+                  onChange={(e) => setCode(e.target.value)}
+                />
+                {error && (
+                  <p className="font-inter text-l1 text-r-900 text-center">
+                    {error}
+                  </p>
+                )}
+              </div>
             </>
           )}
           {!verifying && (
             <>
+            <div className="flex flex-col gap-1">
               <label
                 htmlFor="email"
                 className="font-inter text-bl-600 text-p1 font-medium"
@@ -119,12 +192,17 @@ const PasswordReset = () => {
                 Email
               </label>
               <input
-                type="email"
+                type="text"
                 id="email"
                 placeholder="Your Email"
                 className=" border-1 border-bl-100 rounded-md px-2 h-11 outline-none"
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                  validateEmail(e.target.value);
+                }}
               />
+              {emailError && <p className="font-inter text-l1 text-r-900 text-center">{emailError}</p>}
+            </div>
             </>
           )}
           <button
